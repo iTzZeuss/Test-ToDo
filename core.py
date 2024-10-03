2750c019869c859ed53772064df321c35e9b23e0
UAGoose <arthur.zuiev@gmail.com>
2024-10-03 21:20:58 +0200

    port change

--- core.py
+++ core.py
@@ -10,7 +10,7 @@ def main_page():
 
 if __name__ == '__main__':
     # Get the PORT environment variable from Render (default to 5000 if not set)
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 4000))
     
     # Bind to '0.0.0.0' so the app is accessible externally, and use the port from Render
     app.run(host='0.0.0.0', port=port, debug=config.DEBUG)
