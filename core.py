1bfec88c0a99f2dc608ae9557cda9f5234865bc5
UAGoose <arthur.zuiev@gmail.com>
2024-10-03 21:12:52 +0200

    fixed render

--- core.py
+++ core.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return render_template("index.html")
+    return "Hello Render! I am UAGoose writing from backend"
 
 if __name__ == '__main__':
     app.run(debug=config.DEBUG)
