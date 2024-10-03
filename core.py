c28775e6c4acb09b32443eaa2d71d2b825468dc6
UAGoose <arthur.zuiev@gmail.com>
2024-10-03 21:09:53 +0200

    added requirements

--- core.py
+++ core.py
@@ -4,8 +4,8 @@ import config
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, Flask!"
+def main_page():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=config.DEBUG)
