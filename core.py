24192fa12e09fe7801e306437e3e11ab19d10d50
UAGoose <arthur.zuiev@gmail.com>
2024-10-04 18:57:40 +0200

    added database endpoint

--- core.py
+++ core.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import os 
 import config
+import task_manager as task_mgr
 
 app = Flask(__name__)
 
@@ -8,6 +9,17 @@ app = Flask(__name__)
 def main_page():
     #FORFRONTEND: returning index for user
     return render_template("index.html")
+
+@app.route("/tasklist")
+def return_tasklist():
+    #FORFRONTEND: this endpoint will return list of dictionaries which contains list of all tasks from database.
+    #dict keys: id, header, description, photo_path, status (completed or not)
+    #example of dict: {"id":"0", "header":"Example Task", "description":"once upon a time", "photo_path":"somepath/img", "status":"completed"}
+
+    #fetching list from database
+    task_list = task_mgr.list_tasks()
+    #returning
+    return task_list
 
 if __name__ == '__main__':
     # Just running the app. nothing interesting in here. Mb we will need to setup ssl here
