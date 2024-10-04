0e2f0a63663e4fa3814e4a85fa0894694bb23907
UAGoose <arthur.zuiev@gmail.com>
2024-10-04 18:51:46 +0200

    made database in render

--- core.py
+++ core.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
+    #FORFRONTEND: returning index for user
     return render_template("index.html")
 
 if __name__ == '__main__':
-    # Get the PORT environment variable from Render (default to 5000 if not set)
-    port = int(os.environ.get('PORT', 4000))
-    
-    # Bind to '0.0.0.0' so the app is accessible externally, and use the port from Render
+    # Just running the app. nothing interesting in here. Mb we will need to setup ssl here
+    port = int(os.environ.get('PORT', config.DEFAULT_PORT))
     app.run(host='0.0.0.0', port=port, debug=config.DEBUG)
