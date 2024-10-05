bd81a397da9151fa59834a70749c8c2163ae2abb
UAGoose <arthur.zuiev@gmail.com>
2024-10-05 16:09:51 +0200

    added environmental variable for database password

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 # Run config
 DEBUG = True
 DEFAULT_PORT = 4000
@@ -6,7 +8,8 @@ DEFAULT_PORT = 4000
 DB_HOST = 'dpg-cs01lmpu0jms73e05tfg-a.frankfurt-postgres.render.com'
 DB_NAME = 'tasks_db_0fry'
 DB_USER = 'tasks_db_0fry_user'
-DB_PASSWORD = '0BmyfK6LYgtJ1L1e6tpZMrU2I74uPE53' #make env variable in future to not save in plain text
+DB_PASSWORD = os.getenv('db_password')
+DEBUG_DB_PASSWORD = '0BmyfK6LYgtJ1L1e6tpZMrU2I74uPE53' #when debug is finished delete it
 DB_PORT = 5432
 
 # Paths configuration
