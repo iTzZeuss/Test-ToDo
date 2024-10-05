e5c903e6b2091a789c4c7b3d94563067425d8734
UAGoose <arthur.zuiev@gmail.com>
2024-10-05 15:34:45 +0200

    added `/delete_task` route

--- core.py
+++ core.py
@@ -12,23 +12,36 @@ def main_page():
 
 @app.route("/tasklist")
 def return_tasklist():
-    #FORFRONTEND: this endpoint will return list of dictionaries which contains list of all tasks from database.
+    #FORFRONTEND: this route will return list of dictionaries which contains list of all tasks from database.
     #dict keys: id, header, description, photo_path, status (completed or not)
     #example of dict: {"id":"0", "header":"Example Task", "description":"once upon a time", "photo_path":"somepath/img", "status":"completed"}
 
     #fetching list from database
     task_list = task_mgr.list_tasks()
+    task_mgr.close_connection()
     #returning
     return jsonify(task_list)
 
 @app.route('/taskinfo/<int:id>', methods=['GET'])
 def return_task_info(id):
-    #FORFRONTEDL this enpoint will return single dictionary which contains information about specific task.
+    #FORFRONTEDL this route will return single dictionary which contains information about specific task.
     #Note: include id in request
     #request example for task with id 2: alterapps.xyz/taskinfo/2
 
     taskinfo = task_mgr.get_task(id)
+    task_mgr.close_connection()
+
     return jsonify(taskinfo)
+
+@app.route('/delete_task/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    #FORFRONTEND this route will delete item specified by the url
+    #Note: inclide id in request
+    #request example for task with id 2: alterapps.xyz/delete_task/2
+    task_mgr.delete_task(id)
+    task_mgr.close_connection()
+
+    return 200
 
 if __name__ == '__main__':
     # Just running the app. nothing interesting in here. Mb we will need to setup ssl here
