a951a8448281f3929248d93dc95058d06ead9320
UAGoose <arthur.zuiev@gmail.com>
2024-10-06 11:23:27 +0200

    connection test

--- config.py
+++ config.py
@@ -5,12 +5,7 @@ DEBUG = True
 DEFAULT_PORT = 4000
 
 # Database config
-DB_HOST = 'dpg-cs01lmpu0jms73e05tfg-a.frankfurt-postgres.render.com'
-DB_NAME = 'tasks_db_0fry'
-DB_USER = 'tasks_db_0fry_user'
-DB_PASSWORD = os.getenv('db_password')
-DEBUG_DB_PASSWORD = '0BmyfK6LYgtJ1L1e6tpZMrU2I74uPE53' #when debug is finished delete it
-DB_PORT = 5432
+DB_HOST = os.getenv('db_url')
 
 # Paths configuration
-PHOTOS_FOLDER_PATH = "static/photos/"
+PHOTOS_FOLDER_PATH = "task_photos/"
