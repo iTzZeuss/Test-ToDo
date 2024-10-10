5376e64a28171d2a51f1960865592544127a46a8
John Doe <johndoe@email.com>
2024-10-11 00:55:09 +0300

    goofy stuff sorry its 1 am

--- static/js/taskCreation.js
+++ static/js/taskCreation.js
@@ -1,7 +1,12 @@
+const numOfDivs = 1;
+let txt1 = document.getElementById("task_header").value;
+
 function getDiv () {
     for (i = 1; i <= numOfDivs; i++) {
         let newDiv = document.createElement("div")
-        newDiv.textContent = "" //will do these 2 tmrw
-        newDiv.className = ""
+        newDiv.textContent = txt1
+        newDiv.className = newTask
+        newDiv.style.display = "block";
+
     }
 }
