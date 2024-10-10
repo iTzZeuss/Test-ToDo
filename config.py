dd0ade74b23d709841154d5cf9c39849929eb465
UAGoose <arthur.zuiev@gmail.com>
2024-10-10 19:04:54 +0200

    updated config

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ DEFAULT_PORT = 4000
 # Database config
 DB_HOST = 'ep-dark-field-a2x8dnvu.eu-central-1.aws.neon.tech'
 DB_NAME = 'task_database'
-DB_USER = 'task_database_owner'
+DB_USER = environmental_var("db_usr")
 DB_PASSWORD = environmental_var("db_pwd")
 
 # Paths configuration
