f0ab421cd2e9695f641aa398bb962864c0094940
UAGoose <arthur.zuiev@gmail.com>
2024-10-10 19:51:03 +0200

    made it debug friendly

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ DEFAULT_PORT = 4000
 # Database config
 DB_HOST = 'ep-dark-field-a2x8dnvu.eu-central-1.aws.neon.tech'
 DB_NAME = 'task_database'
-DB_USER = environmental_var("db_usr")
-DB_PASSWORD = environmental_var("db_pwd")
+DB_USER = "task_database_owner" #environmental_var("db_usr")
+DB_PASSWORD = "7DozN5UjTPnh" #environmental_var("db_pwd")
 
 # Paths configuration
 PHOTOS_FOLDER_PATH = "/task_photos/"
